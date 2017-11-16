cc93d7dd9c418dbbb47a6f7a6e4697e56ee3fff3
Hundemeier <hundemeier99@gmail.com>
2017-11-16 17:48:23 +0100

    Minor fixes for release in the readme and setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='sacn',
       version='0.1',
       description='sACN / E1.31 module for easy handling of DMX data over ethernet',
-      url='',
-      author='Thomas Neteler',
+      url='https://www.github.com/Hundemeier/sacn',
+      author='Hundemeier',
       author_email='hundemeier99@gmail.com',
-      license='No License available! No publication!',
+      license='MIT License',
       packages=['sacn'],
       zip_safe=False)
