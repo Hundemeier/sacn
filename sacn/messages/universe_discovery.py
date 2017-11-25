31cfe2450f74fe2c4cd2a522a7408c107051c56e
Hundemeier <hundemeier99@gmail.com>
2017-11-25 15:05:33 +0100

    Added feature: made the callback available via decorator and the callback only gets called, when the data has changed

---
+++ sacn/messages/universe_discovery.py
@@ -0,0 +1,11 @@
+"""
+This class represents an universe discovery packet of the E1.31 Standard.
+"""
+from .root_layer import RootLayer
+
+
+class UniverseDiscoveryPacket(RootLayer):
+    def __init__(self):
+        raise NotImplementedError
+
+        # ToDo: make the class
--- sacn/receiver.py
+++ sacn/receiver.py
@@ -19,13 +19,28 @@ class sACNreceiver:
         # If you try to bind to all interfaces on Windows, no multicast data will arrive.
         self._bindAddress = bind_address
         self._thread = None
-        self.callbacks = []
+        self._callbacks = {}
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         try:
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         except:  # Not all systems support multiple sockets on the same port and interface
             pass
         self.sock.bind((bind_address, bind_port))
+
+    def listen(self, universe: int):
+        """
+        This is a decorator for callbacks that should react on the given universe.
+        The callbacks are getting inherited if the dmx data was changed
+        :param universe: the universe to listen on
+        """
+        def decorator(f: callable):
+            # add callback to the _callbacks list for the universe
+            try:
+                self._callbacks[universe].append(f)
+            except AttributeError:  # try to append
+                self._callbacks[universe] = list(f)
+            return f
+        return decorator
 
     def join_multicast(self, universe: int):
         """
@@ -58,7 +73,7 @@ class sACNreceiver:
         Starts a new thread that handles the input. If a thread is already running, the thread will be restarted.
         """
         self.stop()  # stop an existing thread
-        self._thread = _receiverThread(socket=self.sock, callbacks=self.callbacks)
+        self._thread = _receiverThread(sock=self.sock, callbacks=self._callbacks)
         self._thread.start()
 
     def stop(self):
@@ -76,7 +91,7 @@ class sACNreceiver:
 
 
 class _receiverThread(threading.Thread):
-    def __init__(self, socket: socket.socket, callbacks: list):
+    def __init__(self, sock: socket.socket, callbacks: dict):
         """
         This is a private class and should not be used elsewhere. It handles the while loop running in the thread.
         :param socket: the socket to use to listen. It will not be initalized and only the socket.recv function is used.
@@ -84,12 +99,13 @@ class _receiverThread(threading.Thread):
         :param callbacks: the list with all callbacks
         """
         self.enabled_flag = True
-        self.sock = socket
+        self.sock = sock
         self.callbacks = callbacks
+        self._previousData = {}
         super().__init__(name='sACN input/receiver thread')
 
     def run(self):
-        socket.settimeout(0.1)  # timeout as 100ms
+        self.sock.settimeout(0.1)  # timeout as 100ms
         self.enabled_flag = True
         while self.enabled_flag:
             try:
@@ -100,8 +116,14 @@ class _receiverThread(threading.Thread):
                 tmp_packet = DataPacket.make_data_packet(raw_data)
             except:  # try to make a DataPacket. If it fails just go over it
                 continue
-            for callback in self.callbacks:
-                try:
+
+            # call the listeners for the universe but before check if the data has changed
+            # check if there are listeners for the universe before proceeding
+            if tmp_packet.universe not in self.callbacks.keys():
+                continue
+            if self._previousData[tmp_packet.universe] is None or \
+               self._previousData[tmp_packet.universe] != tmp_packet.dmxData:
+                # set previous data and inherit callbacks
+                self._previousData[tmp_packet.universe] = tmp_packet.dmxData
+                for callback in self.callbacks[tmp_packet.universe]:
                     callback(tmp_packet)
-                except:  # try to call back
-                    pass
