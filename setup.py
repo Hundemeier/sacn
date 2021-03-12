9d45385f8b70c22ec680bae846c13722d44af813
Hundemeier <hundemeier99@gmail.com>
2021-03-12 18:35:19 +0100

    updated version number and changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='sacn',
-    version='1.6.0',
+    version='1.6.1',
     description='sACN / E1.31 module for easy handling of DMX data over ethernet',
     long_description=long_description,
     long_description_content_type="text/markdown",
