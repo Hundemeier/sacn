7b2a77e14c66b9a04e869ced0d0ac80e0d89bc33
Hundemeier <hundemeier99@gmail.com>
2021-03-13 09:32:34 +0100

    added ingore-comments for false-positives of flake8

--- sacn/__init__.py
+++ sacn/__init__.py
@@ -1,7 +1,8 @@
-from sacn.receiver import sACNreceiver, LISTEN_ON_OPTIONS
-from sacn.sender import sACNsender
-from sacn.messages.data_packet import DataPacket
-from sacn.messages.universe_discovery import UniverseDiscoveryPacket
+# re-export the classes available to consumers of this library
+from sacn.receiver import sACNreceiver, LISTEN_ON_OPTIONS  # noqa: F401
+from sacn.sender import sACNsender  # noqa: F401
+from sacn.messages.data_packet import DataPacket  # noqa: F401
+from sacn.messages.universe_discovery import UniverseDiscoveryPacket  # noqa: F401
 
 import logging
 logging.getLogger('sacn').addHandler(logging.NullHandler())
