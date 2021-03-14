cb181e7b11a70d8e8eec3cf0b0c5fb08e4ef328f
Hundemeier <hundemeier99@gmail.com>
2021-03-14 11:34:44 +0100

    added more advanced example

--- example.py
+++ example.py
@@ -6,6 +6,7 @@
 # (e.g. (5, 6, 7, 8, ...) is followed by (8, 9, 10, 11, ...))
 # This is due to the fact, that the sending loop sets the DMX data approx. every 10ms,
 # but the sending thread subsamples with approx. 33ms.
+# The universe that is used to transmit the data is switched from 1 to 2 in between.
 
 import time
 import sacn
@@ -22,25 +23,33 @@ def callback_available(universe, changed):
     print(f'universe {universe}: {changed}')
 
 
-@receiver.listen_on('universe', universe=1)  # listens on universe 1
+@receiver.listen_on('universe', universe=1)  # listens on universes 1 and 2
+@receiver.listen_on('universe', universe=2)
 def callback(packet):  # packet type: sacn.DataPacket
-    print(packet.dmxData[:8])  # print the received DMX data, but only the first 8 values
+    print(f'{packet.universe}: {packet.dmxData[:8]}')  # print the received DMX data, but only the first 8 values
 
 
 receiver.join_multicast(1)
+receiver.join_multicast(2)
 
 
 sender.activate_output(1)  # start sending out data in the 1st universe
 sender[1].multicast = True  # set multicast to True
 
 
-# with 200 value changes and each taking 10ms, this for-loop runs for 2s
-for i in range(0, 200):
-    # set test DMX data that increases its first four values each iteration
-    sender[1].dmx_data = tuple(x % 256 for x in range(i, i + 4))
-    time.sleep(0.01)  # sleep for 10ms
+def send_out_for_2s(universe: int):
+    # with 200 value changes and each taking 10ms, this for-loop runs for 2s
+    for i in range(0, 200):
+        # set test DMX data that increases its first four values each iteration
+        sender[universe].dmx_data = tuple(x % 256 for x in range(i, i + 4))
+        time.sleep(0.01)  # sleep for 10ms
 
-sender.deactivate_output(1)
+
+send_out_for_2s(1)
+sender.move_universe(1, 2)
+send_out_for_2s(2)
+
+sender.deactivate_output(2)
 
 # stop both threads
 receiver.stop()
